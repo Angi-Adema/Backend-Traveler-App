7d15db0bdea9daabb2fd732f81ae720940da6af2
CoderGirl <angi.adema@gmail.com>
2023-08-12 03:58:56 -0600

    moved all files into the root project file

---
+++ routes/api/travellerRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Traveller } = require('../../models');
+
+// GET request to return all traveller data without the associated trips. (The `/api/travellers` endpoint)
+router.get('/', async (req, res) => {
+    try {    
+        const travellerData = await Traveller.findAll();
+        res.status(200).json(travellerData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
