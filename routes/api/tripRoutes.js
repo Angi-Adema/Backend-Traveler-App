cdd65c96c40a90c2fa4175a0040884fb396e1e9a
CoderGirl <angi.adema@gmail.com>
2023-08-23 15:05:32 -0600

    updated tripRourtes.js with a post route to add a new trip

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
+const { Traveller, Trip, Location } = require('../../models');
+
+// POST route to create a new trip.
+router.post('/', async (req, res) => {
+    try {
+        const tripData = await Trip.create(req.body);
+        res.status(200).json(tripData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// DELETE route to delete a trip.
+
 
 module.exports = router;
