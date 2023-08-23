b8de45fd23edf2d1dc01ad4be8b7d3abd7c79588
CoderGirl <angi.adema@gmail.com>
2023-08-23 15:01:05 -0600

    added a delete route to delete a location on locationRoutes.js

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -38,4 +38,24 @@ router.post('/', async (req, res) => {
     }
 });
 
+// DELETE route to delete a location.
+router.delete('/:id', async (req, res) => {
+    try {
+        const locationData = await Location.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        
+        if (!locationData) {
+            res.status(404).json({ message: 'No location found with this id!' });
+            return;
+        }
+        
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
