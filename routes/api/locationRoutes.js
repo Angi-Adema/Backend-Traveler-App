0a97636f6c71fb04d25da52bcb61c50131f53aaa
CoderGirl <angi.adema@gmail.com>
2023-08-23 14:46:52 -0600

    created get route for all vacation locations in locationRoutes.js

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
+const { Location, Trip, Traveller } = require('../../models');
+
+// GET route return all vacation locations.
+router.get('/', async (req, res) => {
+    try {
+        const locationData = await Location.findAll();
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
