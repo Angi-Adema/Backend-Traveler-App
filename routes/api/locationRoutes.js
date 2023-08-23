f5e257b6156c531b94c8f8a4a7d6dc7c6ef133ee
CoderGirl <angi.adema@gmail.com>
2023-08-23 14:54:14 -0600

    added get route for a single location in locationRoutes.js

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -11,4 +11,21 @@ router.get('/', async (req, res) => {
     }
 });
 
+// GET route to return a single location.
+router.get('/:id', async (req, res) => {
+    try {
+        const locationData = await Location.findByPk(req.params.id, {
+            include: [{ model: Traveller, through: Trip, as: 'travellers_locations' }]
+        });
+        
+        if (!locationData) {
+            res.status(404).json({ message: 'No location found with this id!' });
+            return;
+        }
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
