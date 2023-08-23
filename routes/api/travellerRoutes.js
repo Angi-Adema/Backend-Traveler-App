6a53be378f228ad3e2fcade8563c44098cf33c5e
CoderGirl <angi.adema@gmail.com>
2023-08-23 14:33:45 -0600

    added the get route for a single traveller

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Traveller } = require('../../models');
+const { Traveller, Trip, Location } = require('../../models');
 
 // GET request to return all traveller data without the associated trips. (The `/api/travellers` endpoint)
 router.get('/', async (req, res) => {
@@ -10,5 +10,41 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// GET request to return a single traveller.
+router.get('/:id', async (req, res) => {
+    try {
+        const travellerData = await Traveller.findByPk(req.params.id, {
+            include: [{ model: Location, through: Trip, as: 'trips_scheduled' }]
+        });
+        if (!travellerData) {
+            res.status(404).json({ message: 'No traveller found with this id!' });
+            return;
+        }
+        res.status(200).json(travellerData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const travellerData = await Traveller.findByPk(req.params.id, {
+//            include: [{ model: Location, through: Trip, as: 'trips_scheduled' }] 
+//         });
+
+//         if (!travellerData) {
+//             res.status(404).json({ message: 'No traveller found with this id!' });
+//             return;
+//         }
+
+//         res.status(200).json(travellerData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
