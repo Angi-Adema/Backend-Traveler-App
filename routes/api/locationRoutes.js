4e75a40b1647b70b9ea5f0b3b030f6a67224a4fc
CoderGirl <angi.adema@gmail.com>
2023-08-23 14:58:37 -0600

    added a post route to create a new location in locationRoutes.js

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -28,4 +28,14 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// POST route to create a new location.
+router.post('/', async (req, res) => {
+    try {
+        const locationData = await Location.create(req.body);
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 module.exports = router;
