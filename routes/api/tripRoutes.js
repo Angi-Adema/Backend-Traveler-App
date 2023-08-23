3a85e2270f43c9e39b9009ae51c18d5051e5e5a9
CoderGirl <angi.adema@gmail.com>
2023-08-23 15:10:44 -0600

    added a delete route to delete a trip in tripRoutes.js

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -12,6 +12,21 @@ router.post('/', async (req, res) => {
 });
 
 // DELETE route to delete a trip.
-
+router.delete('/:id', async (req, res) => {
+    try {
+        const tripData = await Trip.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!tripData) {
+            res.status(404).json({ message: 'No trip found with this id!' });
+            return;
+        }
+        res.status(200).json(tripData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
